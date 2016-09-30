80f81174038d3d8f9ba43a6ab58c559486ebda78
zhangxueyuan <645054538@qq.com>
2016-09-30 17:31:47 +0800

    markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def markdown(text)
+    options = {
+        :autolink => true,
+        :space_after_headers => true,
+        :fenced_code_blocks => true,
+        :no_intra_emphasis => true,
+        :hard_wrap => true,
+        :strikethrough =>true
+    }
+    markdown = Redcarpet::Markdown.new(HTMLWithCodeRay,options)
+    markdown.render(h(text)).html_safe
+  end
+
+
+  class HTMLWithCodeRay < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div(:line_numbers => :table)
+    end
+  end
+
 end
